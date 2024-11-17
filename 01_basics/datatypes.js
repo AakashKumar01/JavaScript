efc19c25e142a2cc4913a35b198bd69af0ff62bc
AakashKumar01 <118280676+AakashKumar01@users.noreply.github.com>
2024-11-17 06:41:27 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("aakash")
+
+
+let name = "aakash"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint =>  limited only by the available system memory.
+// Declaration: To create a BigInt, append the letter n to an integer literal:
+// Ex:  const bigIntValue = 123456789012345678901234567890n; 
+// string => ""
+// boolean => true/false
+// null => standalone value iska type object hai
+// undefined => undefine ka type undefine hota hai
+// symbol => unique
+
+
+// object
+// Array
+// RegExp
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
